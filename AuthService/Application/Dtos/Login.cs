f6890604fa6df52d9369bbea295045d15497f4f3
Pedro Jesus <Pedromra.04@gmail.com>
2025-04-03 20:40:16 -0300

    feat: started authservice

---
+++ AuthService/Application/Dtos/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Application.Dtos
+{
+	public class Login
+	{
+
+		[Required]
+		public string RegistrationNumber { get; set; }
+
+		[Required]
+		public required string Password { get; set; }
+
+	}
+}
---
+++ AuthService/Application/Dtos/Signup.cs
@@ -0,0 +1,21 @@
+﻿using Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Application.Dtos
+{
+	public class Signup
+	{
+		[Required]
+		public required string Name { get; set; }
+
+		public string Specialty { get; set; }
+
+		[Required]
+		public required string RegistrationNumber { get; set; }
+
+		public double? ClaimsRate { get; set; }
+
+		[Required]
+		public RiskStatus RiskStatus { get; set; }
+	}
+}
